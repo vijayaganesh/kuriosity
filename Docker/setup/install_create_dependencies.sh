2f62b1e81aeba9b05728cdcf580199fdf7041f90
vijayaganesh <mvijayaganesh94@gmail.com>
2018-09-02 15:39:15 -0400

    Updated docker file with realsense setup

--- Docker/setup/install_create_dependencies.sh
+++ Docker/setup/install_create_dependencies.sh
@@ -3,6 +3,16 @@
 # Creating a seperate workspace for create libraries as they can be built only using catkin build
 . /opt/ros/kinetic/setup.sh
 
+mkdir -p /home/kuriosity/catkin_ws/src
+cd /home/kuriosity/catkin_ws/src/
+git clone https://github.com/intel-ros/realsense.git
+catkin_init_workspace 
+cd ..
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source /home/kuriosity/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 mkdir -p /home/kuriosity/create_ws/src
 cd /home/kuriosity/create_ws
 catkin init
